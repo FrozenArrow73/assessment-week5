e2850f2d617d363cc98d3a4e9e5a742ca886efe8
Derek Wheeler <frozenarrow73@gmail.com>
2023-03-17 12:22:18 -0600

    completed step 2

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,14 @@
-
+require('dotenv').config()
+const {CONNECTION_STRING} = process.env
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(ONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+  })
 
 module.exports = {
     seed: (req, res) => {
